be13576c8cc9c3fbfa4aeeef22f8f84f2d6e15a6
Javier Pollak <javi.po.123@gmail.com>
2022-08-11 09:51:35 -0400

    Fix go path and luarocks path not set

--- shell/rc.d/50-luarocks.sh
+++ shell/rc.d/50-luarocks.sh
@@ -1,3 +1,7 @@
 export LUA_ROCKS_TREE="$XDG_DATA_HOME/luarocks"
 alias luarocks='luarocks --tree $LUA_ROCKS_TREE'
 export PATH="$LUA_ROCKS_TREE/bin:$PATH"
+
+if command -v luarocks >/dev/null; then
+    eval "$(luarocks path)"
+fi
