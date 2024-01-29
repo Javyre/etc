c9fdd56a2df8a79eebc99194c9be616899d0bf2e
Javier A. Pollak <javi.po.123@gmail.com>
2024-01-29 11:43:38 -0500

    Use emacs bindings in zsh

--- shell/rc.d/50-zsh.sh
+++ shell/rc.d/50-zsh.sh
@@ -1,6 +1,14 @@
 if ! [ -n "$ZSH_VERSION" ]; then
-    return
+	return
 fi
 
-bindkey "^P" up-line-or-history
-bindkey "^N" down-line-or-history
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+bindkey -e
+bindkey "^P" up-line-or-search
+bindkey "^N" down-line-or-search
+
+if command -v exa >/dev/null; then
+	alias ls=exa
+fi
