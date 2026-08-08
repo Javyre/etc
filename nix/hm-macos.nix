eebe350c8a5792b20a2120e077d4dbfdbd1aae09
Javier A. Pollak <javi.po.123@gmail.com>
2026-08-08 19:04:29 -0400

    package crap for macOS

--- nix/hm-macos.nix
+++ nix/hm-macos.nix
@@ -7,6 +7,28 @@
 let
   ln = config.lib.jv.ln;
   xnu = pkgs.apple-sdk.sourceRelease "xnu";
+  crap = pkgs.stdenv.mkDerivation {
+    pname = "crap";
+    version = "0.1.0";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "D-Berg";
+      repo = "crap";
+      rev = "9c5f83b8776af387c2cd355ab74049fbd0934b86";
+      hash = "sha256-IVI1utI+l5eCSzcxibKj89RzgiXRKeEZINhF1Ok2EJw=";
+    };
+
+    nativeBuildInputs = [ pkgs.zig_0_16 ];
+    buildInputs = [ pkgs.apple-sdk ];
+
+    meta = {
+      description = "Counter Reading Acquisition Platform";
+      homepage = "https://github.com/D-Berg/crap";
+      license = lib.licenses.mit;
+      platforms = lib.platforms.darwin;
+      mainProgram = "crap";
+    };
+  };
   lauka = pkgs.stdenv.mkDerivation {
     pname = "lauka";
     version = "unstable-2026-01-12";
@@ -51,6 +73,7 @@ in
     aerospace
     # jankyborders
     raycast
+    crap
     lauka
   ];
 
